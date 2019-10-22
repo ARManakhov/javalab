b9a21d80f1c3352f23abd21956ba12bd7ae5b5b7
ARManakhov <sheih000@gmail.com>
2019-10-22 10:51:12 +0300

    tomcat with maven

---
+++ hw_3 (tomcat)/src/main/java/ru/sirosh/Downloader.java
@@ -0,0 +1,49 @@
+package ru.sirosh;
+
+import ru.sirosh.Config;
+import ru.sirosh.MultipleDownloader;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Downloader extends HttpServlet {
+    Map<String, List<String>> cookiesVals;
+    String cookieString = "token";
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        cookiesVals = (Map<String, List<String>>) getServletContext().getAttribute("cookiesVals");
+        Cookie[] cookies = req.getCookies();
+        List<String> urls = null;
+        boolean cookieFound = false;
+        if (cookies !=null){
+            for (Cookie cookie : cookies) {
+                if (cookiesVals != null && cookie.getName().equals(cookieString) && cookiesVals.containsKey(cookie.getValue())) {
+                    urls = cookiesVals.get(cookie.getValue());
+                    String filesPath = System.getProperty("catalina.home") + File.separator + "data" + File.separator + cookie.getValue();
+                    File savedir = new File(filesPath);
+                    if (!savedir.exists()){
+                        savedir.mkdirs();
+                    }
+                    System.out.println("savepath " + filesPath);
+
+                    new MultipleDownloader(new Config(filesPath,urls.toArray(new String[urls.size()]))).download();
+                    cookiesVals.put(cookie.getValue(), new ArrayList<String>());
+                }
+            }
+        }
+        if (urls != null){
+            System.out.println(urls);
+        }
+        resp.setStatus(HttpServletResponse.SC_FOUND);//302
+        resp.setHeader("Location", "/");
+    }
+}
